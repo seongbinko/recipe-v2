b5ccc10af45abd2257782926833ef382e2305456
seongbinko <wnrhd1082@gmail.com>
2020-12-21 20:48:12 +0900

    코드 정리 및 수정

--- src/main/java/com/seongbindb/recipe/domain/recipes/RecipesDto.java
+++ src/main/java/com/seongbindb/recipe/domain/recipes/RecipesDto.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.apache.ibatis.type.Alias;
 
 import java.util.Date;
 
@@ -14,16 +13,11 @@ import java.util.Date;
 @ToString
 public class RecipesDto {
 
-    private int rn;
     private int recipeNo;
     private String userId;
-    private String nickName;
     private String recipeName;
     private Date createDate;
     private Date modDate;
     private String status;
     private int categoryNo;
-    private int cnt;
-    private String thumbnailImg;
-
 }
--- src/main/java/com/seongbindb/recipe/service/RecipeService.java
+++ src/main/java/com/seongbindb/recipe/service/RecipeService.java
@@ -3,6 +3,7 @@ package com.seongbindb.recipe.service;
 import com.seongbindb.recipe.domain.recipes.RecipesDto;
 import com.seongbindb.recipe.domain.recipes.RecipeRepository;
 
+import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,20 +12,18 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.*;
 
 @Service
+@RequiredArgsConstructor
 @MapperScan(basePackages = "com.seongbindb.recipe.domain.recipes")
 public class RecipeService {
 
-    @Autowired
-    private RecipeRepository recipeRepository;
+    private final RecipeRepository recipeRepository;
 
     @Transactional(readOnly = true)
     public Map<String, Object> search() throws Exception {
-            List<RecipesDto> recipes = recipeRepository.search();
 
-            Map<String, Object> result = new HashMap<String, Object>();
-            result.put("recipes", recipes);
+            Map<String, Object> result = new HashMap();
+            result.put("recipes", recipeRepository.search());
 
             return result;
-
     }
 }
