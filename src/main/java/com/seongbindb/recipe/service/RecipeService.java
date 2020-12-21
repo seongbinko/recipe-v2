534a8e8283037348275cf77fcfaa25c21511c468
seongbinko <wnrhd1082@gmail.com>
2020-12-21 20:53:35 +0900

    edit code

--- src/main/java/com/seongbindb/recipe/service/RecipeService.java
+++ src/main/java/com/seongbindb/recipe/service/RecipeService.java
@@ -1,15 +1,13 @@
 package com.seongbindb.recipe.service;
 
-import com.seongbindb.recipe.domain.recipes.RecipesDto;
 import com.seongbindb.recipe.domain.recipes.RecipeRepository;
-
 import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -19,11 +17,10 @@ public class RecipeService {
     private final RecipeRepository recipeRepository;
 
     @Transactional(readOnly = true)
-    public Map<String, Object> search() throws Exception {
+    public Map<String, Object> search() {
 
-            Map<String, Object> result = new HashMap();
-            result.put("recipes", recipeRepository.search());
-
-            return result;
+        Map<String, Object> result = new HashMap();
+        result.put("recipes", recipeRepository.search());
+        return result;
     }
 }
