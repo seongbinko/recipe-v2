8a78f7fc2ab230692700ed972992583296db82dd
seongbinko <wnrhd1082@gmail.com>
2021-01-02 17:35:07 +0900

    ### 2020.01.02 * 기존의 master를 local_master branch로 변경 * local_01 branch를 master로 변경 * thymeleaf로 회원가입 뷰 생성 (bootstrap4 이용) * 계정 관련 컨트롤러 생성 (스프링 시큐리티 적용) 및 테스트(Junit5) * nav, footer 뷰 생성 * 로고 이미지 등록

--- README.md
+++ README.md
@@ -12,8 +12,19 @@
    - JUnit4 => JUnit5 전환 및 모든 api를 TDD 방식으로 변경한다.
 
 ## 개발 일지
+
 ### 2020.12.20
+
 * recipe-v2 project 생성 (https://github.com/seongbinko/recipe-v2)
 * thymeleaf index.html 생성
-* controller 테스트 수행 ()  
+* controller 테스트 수행 ()
 * postgreSql mybatis 연동
+
+### 2020.01.02
+
+* 기존의 master를 local_master branch로 변경
+* local_01 branch를 master로 변경
+* thymeleaf로 회원가입 뷰 생성 (bootstrap4 이용)
+* 계정 관련 컨트롤러 생성 (스프링 시큐리티 적용) 및 테스트(Junit5)
+* nav, footer 뷰 생성
+* 로고 이미지 등록
---
+++ src/test/java/com/seongbindb/recipe/account/AccountControllerTest.java
@@ -0,0 +1,31 @@
+package com.seongbindb.recipe.account;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("회원 가입 화면 보이는지 테스트")
+    @Test
+    void signUpForm() throws Exception {
+        mockMvc.perform(get("/sign-up"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"))
+                .andExpect(model().attributeExists("signUpForm"));
+    }
+
+}
