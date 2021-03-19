f5ecde1b3ec0cc5e47d69b3165e15273c35348c0
seongbinko <wnrhd1082@gmail.com>
2021-03-19 22:11:15 +0900

    ### 2021.03.19
    1. 프로젝트 파일 war로 배포 및 동작 확인
       - jsp는 스프링부트에서 jar로 배포할 시 동작하지 않기 때문

--- src/main/java/com/seongbindb/recipe/App.java
+++ src/main/java/com/seongbindb/recipe/App.java
@@ -5,6 +5,8 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -13,11 +15,13 @@ import javax.sql.DataSource;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.seongbindb.recipe.mapper"})
-public class App {
+public class App extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+
+    @Override protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { return builder.sources(App.class); }
 
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
