6abd032d950140755e60fdbc7cffe49b22a2049a
seongbinko <wnrhd1082@gmail.com>
2021-04-19 14:54:43 +0900

    ### 2021.04.18
    application-real.properties 추가 및 deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,4 +33,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar -Dspring.config.location=classpath:/application.properties,/home/ubuntu/app/application-auth.properties $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application.properties,/home/ubuntu/app/application-auth.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
